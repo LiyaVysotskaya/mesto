117a3b9be31a0f2ae4f38847de4d951835456822
Liya Vysotskaya <Imredrat@yandex.ru>
2023-03-26 23:05:27 +0300

    feat: added popup__place, rename inputs (26 мар 2023 г. 23:05:27)

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,52 @@
+const form = document.querySelector('.form');
+
+// profile edit variables
 const editBtn = document.querySelector('.profile__edit-button');
-const addBtn = document.querySelector('.profile__add-button');
-const closeBtn = document.querySelector('.popup__close-button');
-const popup = document.querySelector('.popup');
-const form = document.querySelector('.edit');
+const closeEditBtn = document.querySelector('.popup__close-button_edit');
+const formEdit = document.querySelector('.popup__profile');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const editName = document.querySelector('.edit__input_profile_name');
-const editDescription = document.querySelector('.edit__input_profile_description');
+const editName = document.querySelector('.form__input_type_name');
+const editDescription = document.querySelector('.form__input_type_description');
 
-function openEditWindow() {
+// profile add-card variables
+const addBtn = document.querySelector('.profile__add-button');
+const formAdd = document.querySelector('.popup__place');
+const closeAddCardBtn = document.querySelector('.popup__close-button_add-card');
+
+// popup opening function
+function openPopup(popup) {
   popup.classList.add('popup_opened');
+}
+
+// profile editing opening function
+function openEditWindow() {
+  openPopup(formEdit);
   editName.value = profileName.textContent;
   editDescription.value = profileDescription.textContent;
 }
 
-function closeEditWindow() {
+// function to open adding cards
+function openAddWindow() {
+  openPopup(formAdd);
+}
+
+// popup closing function
+function closePopupWindow(popupSelector) {
+  const popup = document.querySelector(popupSelector);
   popup.classList.remove('popup_opened');
 }
 
+// form submission handling function
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = editName.value;
   profileDescription.textContent = editDescription.value;
-  closeEditWindow();
+  closePopupWindow();
 }
 
 editBtn.addEventListener('click', openEditWindow);
-closeBtn.addEventListener('click', closeEditWindow);
+addBtn.addEventListener('click', openAddWindow);
+closeEditBtn.addEventListener('click', ()=>closePopupWindow('.popup__profile'));
+closeAddCardBtn.addEventListener('click', ()=>closePopupWindow('.popup__place'));
 form.addEventListener('submit', handleFormSubmit);
